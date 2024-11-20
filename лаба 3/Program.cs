e9699fddbc72b4eb43cba2fa2afc35a7985a502b
Kaiser-Ai612 <khoshin183@gmail.com>
2024-11-20 23:13:13 +0300

    Update Program.cs

--- лаба 3/Program.cs
+++ лаба 3/Program.cs
@@ -29,8 +29,8 @@ namespace ConsoleApp1
             int[] daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
             int dayOfYear = int.Parse(Console.ReadLine());
             // Определение месяца и дня
-            int month = 0;
-            int day = 0;
+            byte month = 0;
+            byte day = 0;
             for (int i = 0; i < daysInMonth.Length; i++)
             {
                if (dayOfYear <= daysInMonth[i])
@@ -51,25 +51,45 @@ namespace ConsoleApp1
 пользователем. Если число меньше 1 или больше 365, программа должна вырабатывать
 исключение, и выдавать на экран сообщение.
              */
-            if (dayOfYear < 1)
+            Console.WriteLine("напишите чисо от 1 до 365");
+            int dayOfYear4 = int.Parse(Console.ReadLine());
+            byte month4 = 0;
+            byte day4 = 0;
+            if (dayOfYear4 < 1)
             {
                 Console.WriteLine("число не может быть меньше 1");
             }
-            else if (dayOfYear > 365)
+            else if (dayOfYear4 > 365)
             {
                 Console.WriteLine("число не может быть больше 365");
             }
+            for (int i = 0; i < daysInMonth.Length; i++)
+            {
+               if (dayOfYear4 <= daysInMonth[i])
+               {
+                   month4 = i + 1; // Месяцы начинаются с 1
+                   day4 = dayOfYear4; // Остаток - это день в месяце
+                   break;
+               }
+               else
+               {
+                   dayOfYear4 -= daysInMonth[i]; // Уменьшаем номер дня на количество дней в текущем месяце
+               }
+            }
 
+                // Вывод результата
+            Console.WriteLine($"{day4} {GetMonthName(month4)}.");
+            
             /* Домашнее задание 4.1 Изменить программу из упражнений 4.1 и 4.2 так, чтобы она
 учитывала год (високосный или нет). Год вводится с экрана. (Год високосный, если он
 делится на четыре без остатка, но если он делится на 100 без остатка, это не високосный
 год. Однако, если он делится без остатка на 400, это високосный год.)
              */
             Console.WriteLine("введите год потом число");
-            int year = int.Parse(Console.ReadLine());
+            short year = int.Parse(Console.ReadLine());
             //int dayOfYear1 = int.Parse(Console.ReadLine());
-            int month1 = 0;
-            int day1 = 0;
+            byte month1 = 0;
+            byte day1 = 0;
             
             int[] daysInMonth2 = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
             if (year % 4 == 0 && year % 100 == 0) //не високосный
